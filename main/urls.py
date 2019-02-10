fe002f149c4d52f06caa5ac534ac532d3fe16a34
Alexis Macbook <alexguibourge@gmail.com>
2019-02-10 19:09:19 +0100

    shuffle feature

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ from . import views
 # Match the urls to the views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shuffle', views.shuffle, name='shuffle'),
     path('<int:post_id>/star', views.star, name='star'),
 ]
--- main/views.py
+++ main/views.py
@@ -15,6 +15,15 @@ def index(request):
             'posts': posts.all(), 
         }
     return HttpResponse(template.render(context,request))
+    
+# Return the posts in a shuffled order
+def shuffle(request): 
+    posts = Post.objects.order_by('?')
+    template = loader.get_template('main/base_content.html')
+    context = {
+     		'posts': posts.all(), 
+     	}
+    return HttpResponse(template.render(context,request))
 
 
 # View incrementing the star counters
@@ -23,3 +32,5 @@ def star(request, post_id):
     post.likes += 1
     post.save()
     return(HttpResponse(post.likes))
+    
+
